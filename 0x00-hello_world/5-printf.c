bb0865aa42195ac40b60295d808314ca5cb81ee4
salah-koulal <salah.rajawi289@gmail.com>
2023-03-16 09:51:57 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+/**
+ *use printf to prints with proper grammar, but the outcome is a piece of art,
+ *Return: 0 if exited properly
+ */
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a piece of art, \n");
+     return (0)
+}
