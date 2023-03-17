a517ec106906e66b3c4e511a8520670ea811181c
salah-koulal <salah.rajawi289@gmail.com>
2023-03-17 11:42:05 -0700

    Variables if else while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase and in uppercase
+ * followed by a new line
+ * return: 0 always
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
