a11027aa2b6a7a3869bbe3749654fb6c766da39b
salah-koulal <salah.rajawi289@gmail.com>
2023-03-21 12:55:01 -0700

    function nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - use putchar
- * return : 0 success
+ * main -  prints _putchar
+ * Return: Always 0
  */
-
 int main(void)
 {
 	_putchar('_');
@@ -16,5 +15,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
