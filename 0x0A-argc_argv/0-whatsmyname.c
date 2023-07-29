2ec486351346ece22157e706148471e61203d7d2
salah-koulal <salah.rajawi289@gmail.com>
2023-07-29 15:47:09 +0100

    argv my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,11 +9,10 @@
  * Return: Always 0 (success)
  */
 
-int main(int __attribute__((unused)) argc, char* argv[])
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-    // print the name of the program
-    printf("%s \n",argv[0]);    
-    
-    return 0;
-}
+        /* print the name of the program */
+        printf("%s \n", argv[0]);
 
+        return 0;
+}
