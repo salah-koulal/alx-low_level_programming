133b885da9b3fecaebf1fcfa0115ef20c029c3aa
salah-koulal <salah.rajawi289@gmail.com>
2023-07-29 15:26:10 +0100

    argv my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * @argc: The number of arguments passed to the program (unused)
+ * @argv: An array of pointers to the command-line arguments
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(int __attribute__((unused)) argc, char* argv[])
+{
+    // print the name of the program
+    printf("%s \n",argv[0]);    
+    
+    return 0;
+}
+
