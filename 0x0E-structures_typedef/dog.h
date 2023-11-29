9b714c37a6d8bc57692c21f5413d4d7eb57dba29
Salah <user@Users-MacBook-Pro.local>
2023-11-29 13:11:10 +0100

    HeaderFile

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,36 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure representing a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * This structure represents a dog, with a name, age, and owner. It is used
+ * to hold information about individual dogs in a program. The structure is
+ * defined using three members: a character pointer for the name, a float for
+ * the age, and another character pointer for the owner.
+ */
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
+
+
+#endif /* DOG_H */
