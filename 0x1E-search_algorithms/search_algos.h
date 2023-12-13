278aa8bb7e0c60a57aa6e00a654adef20b988bee
salah-koulal <salah.rajawi289@gmail.com>
2023-12-13 01:13:19 +0100

    headerfile

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+#include <stddef.h>
+#include <stdio.h>
+
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+void print_array(int *array, size_t size);
+int _advanced_binary(int *array, size_t left, size_t right, int value);
+int advanced_binary(int *array, size_t size, int value);
+size_t min(size_t a, size_t b);
+int jump_search(int *array, size_t size, int value);
+#endif
