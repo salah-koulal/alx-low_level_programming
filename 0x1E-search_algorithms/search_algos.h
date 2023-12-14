8c658ad486977a42407a513bdecacd1010914e3c
salah-koulal <salah.rajawi289@gmail.com>
2023-12-14 01:31:13 +0100

    header updated

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,6 @@ int advanced_binary(int *array, size_t size, int value);
 size_t min(size_t a, size_t b);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
 
 #endif
